030a32682bfdc09455918a5be99e7abe4659f30e
Shaik Kamil <shaikm.kamil@gmail.com>
2022-01-10 16:44:41 -0500

    commit tree 756d7a74a23a7640a21fd1ecba8123c045f36082

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2 
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) ;
+
+let num3 = 16
+let num4 = 4
+
+const mod = num3 / num4;
+
+var max = Math.max(1,2,3,4,5,6,7,8,9,10,20);
+max;
